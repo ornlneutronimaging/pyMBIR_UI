2d8d2a6b63889bb8712a7d38c4a27d1940d99f32
JeanBilheux <bilheuxjm@ornl.gov>
2021-06-25 08:13:44 -0400

    crop region (infinite lines). this refs #4

--- src/pyMBIR_UI/crop_handler.py
+++ src/pyMBIR_UI/crop_handler.py
@@ -46,6 +46,10 @@ class CropHandler:
                 self.parent.crop_from_slice_item = None
                 self.parent.ui.crop_image_view.removeItem(self.parent.crop_to_slice_item)
                 self.parent.crop_to_slice_item = None
+                self.parent.ui.crop_image_view.removeItem(self.parent.crop_from_slice_label_item)
+                self.parent.crop_from_slice_label_item = None
+                self.parent.ui.crop_image_view.removeItem(self.parent.crop_to_slice_label_item)
+                self.parent.crop_to_slice_label_item = None
 
     def file_index_changed(self):
         file_index_selected = self.parent.ui.crop_file_index_horizontalSlider.value()
@@ -64,12 +68,10 @@ class CropHandler:
 
         self.parent.crop_from_slice_item = pg.InfiniteLine([0, 0],
                                                            pen=_pen,
-                                                           # label="{}".format(from_slice),
                                                            angle=0,
                                                            span=(0, 1),
                                                            movable=True,
-                                                           bounds=[0, self.parent.crop_image_height],
-                                                           labelOpts={'position': 0.9})
+                                                           bounds=[0, self.parent.crop_image_height-1])
         self.parent.crop_from_slice_label_item = pg.TextItem(text=f"{from_slice}",
                                                              anchor=(0, 1))
         self.parent.crop_from_slice_label_item.setPos(self.parent.crop_image_width, from_slice)
@@ -78,14 +80,17 @@ class CropHandler:
         self.parent.crop_from_slice_item.sigPositionChanged.connect(self.parent.crop_from_slice_changed)
 
         self.parent.crop_to_slice_item = pg.InfiniteLine([0, to_slice],
-                                        pen=_pen,
-                                        angle=0,
-                                        span=(0, 1),
-                                        movable=True,
-                                        bounds=[0, self.parent.crop_image_height],
-                                        labelOpts={'position': 0.9})
+                                                         pen=_pen,
+                                                         angle=0,
+                                                         span=(0, 1),
+                                                         movable=True,
+                                                         bounds=[0, self.parent.crop_image_height-1])
+        self.parent.crop_to_slice_label_item = pg.TextItem(text=f"{to_slice}",
+                                                           anchor=(0, 1))
+        self.parent.crop_to_slice_label_item.setPos(self.parent.crop_image_width, to_slice)
+        self.parent.ui.crop_image_view.addItem(self.parent.crop_to_slice_label_item)
         self.parent.ui.crop_image_view.addItem(self.parent.crop_to_slice_item)
-        self.parent.crop_from_slice_item.sigPositionChanged.connect(self.parent.crop_from_slice_changed)
+        self.parent.crop_to_slice_item.sigPositionChanged.connect(self.parent.crop_from_slice_changed)
 
     def crop_slice_moved(self):
         if self.parent.crop_to_slice_label_item:
@@ -93,15 +98,20 @@ class CropHandler:
             self.parent.ui.crop_image_view.removeItem(self.parent.crop_to_slice_label_item)
 
         from_slice_item = self.parent.crop_from_slice_item
-        from_value = np.int(from_slice_item.value())
+        from_value = np.int(from_slice_item.value())+1
         self.parent.crop_from_slice_label_item = pg.TextItem(text=f"{from_value}",
                                                              anchor=(0, 1))
         self.parent.crop_from_slice_label_item.setPos(self.parent.crop_image_width, from_value)
         self.parent.ui.crop_image_view.addItem(self.parent.crop_from_slice_label_item)
 
         to_slice_item = self.parent.crop_to_slice_item
-        to_value = np.int(to_slice_item.value())
+        to_value = np.int(to_slice_item.value())+1
         self.parent.crop_to_slice_label_item = pg.TextItem(text=f"{to_value}",
-                                                             anchor=(0, 1))
+                                                           anchor=(0, 1))
         self.parent.crop_to_slice_label_item.setPos(self.parent.crop_image_width, to_value)
         self.parent.ui.crop_image_view.addItem(self.parent.crop_to_slice_label_item)
+
+        real_from_value = np.min([from_value, to_value])
+        real_to_value = np.max([from_value, to_value])
+        self.parent.ui.crop_from_slice_label.setText(str(real_from_value))
+        self.parent.ui.crop_to_slice_label.setText(str(real_to_value))
