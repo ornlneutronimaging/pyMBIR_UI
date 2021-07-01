88d69e8f1cc6d51b863b06003ebfe774a057c8d4
JeanBilheux <bilheuxjm@ornl.gov>
2021-07-01 07:55:58 -0400

    initialization of pyqtgraph. this refs #11

--- src/pyMBIR_UI/tilt/setup_0_180_degree_handler.py
+++ src/pyMBIR_UI/tilt/setup_0_180_degree_handler.py
@@ -1,6 +1,8 @@
 from qtpy.QtWidgets import QDialog
+from qtpy.QtWidgets import QVBoxLayout
 from .. import load_ui
 import os
+import pyqtgraph as pg
 
 
 class Setup0180DegreeHandler(QDialog):
@@ -15,5 +17,33 @@ class Setup0180DegreeHandler(QDialog):
         self.ui = load_ui(ui_full_path, baseinstance=self)
         self.setWindowTitle("0 and 180 degrees images setup")
 
+        self.initialization()
+
+    def initialization(self):
+
+        # pyqtgraph
+        self.ui.image_view = pg.ImageView(view=pg.PlotItem())
+        self.ui.image_view.ui.roiBtn.hide()
+        self.ui.image_view.ui.menuBtn.hide()
+        image_layout = QVBoxLayout()
+        image_layout.addWidget(self.ui.image_view)
+        self.ui.widget.setLayout(image_layout)
+
+    def slider_clicked(self):
+        pass
+
+    def slider_moved(self, value):
+        pass
+
+    def image_0_degree_changed(self, value):
+        pass
+
+    def image_180_degree_changed(self, value):
+        pass
+
+    def ok_clicked(self):
+        self.close()
+
     def closeEvent(self):
         pass
+
