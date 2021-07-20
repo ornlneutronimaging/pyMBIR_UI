4a30bd20154f1eb32b181f7afc07e6747b10bc35
JeanBilheux <bilheuxjm@ornl.gov>
2021-07-20 09:02:45 -0400

    connecting the advanced settings window widgets. this refs #13

--- src/pyMBIR_UI/advanced_settings_handler.py
+++ src/pyMBIR_UI/advanced_settings_handler.py
@@ -1,5 +1,5 @@
 import os
-from qtpy.QtWidgets import QMainWindow
+from qtpy.QtWidgets import QMainWindow, QDialog
 
 from . import load_ui
 
@@ -35,12 +35,28 @@ class AdvancedSettingsPasswordHandler(QMainWindow):
         self.close()
 
 
-class AdvancedSettingsHandler(QMainWindow):
+class AdvancedSettingsHandler(QDialog):
 
     def __init__(self, parent=None):
         self.parent = parent
-        super(QMainWindow, self).__init__(parent)
+        super(QDialog, self).__init__(parent)
         ui_full_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),
                                     os.path.join('ui',
                                                  'advanced_settings.ui'))
         self.ui = load_ui(ui_full_path, baseinstance=self)
+
+    def vox_clicked(self):
+        same_behavior_state = self.ui.vox_xy_z_radioButton.isChecked()
+        same_behavior_widgets = [self.ui.vox_xy_z_label,
+                                 self.ui.vox_xy_z_doubleSpinBox]
+        not_same_behavior_widgets = [self.ui.vox_xy_label,
+                                     self.ui.vox_xy_doubleSpinBox,
+                                     self.ui.vox_z_label,
+                                     self.ui.vox_z_doubleSpinBox]
+        for _ui in same_behavior_widgets:
+            _ui.setEnabled(same_behavior_state)
+        for _ui in not_same_behavior_widgets:
+            _ui.setEnabled(not same_behavior_state)
+
+    def det_clicked(self):
+        pass
