832dc6bdf3e83b280e38d64ed90cb18bbc9a5f53
Singanallur Venkatakrishnan <usv@ornl.gov>
2021-09-09 10:13:47 -0400

    Adding imports and commenting out redundant code

--- src/pyMBIR_UI/venkat_function.py
+++ src/pyMBIR_UI/venkat_function.py
@@ -1,14 +1,20 @@
-from qtpy.QtCore import QObject, Signal
-import time
 import numpy as np
 import logging
-
 from qtpy.QtCore import QObject, QThread, Signal
-import astra
-import time
-from pyMBIR.utils import createTransmission,rmse
-from pyMBIR.geometry import generateAmatrix
-from pyMBIR.reconEngine import MBIR
+#import astra
+#import time
+#from pyMBIR.utils import createTransmission,rmse
+#from pyMBIR.geometry import generateAmatrix
+#from pyMBIR.reconEngine import MBIR
+from tomopy import remove_stripe_fw
+from dxchange.writer import write_tiff_stack
+from pyMBIR.reconEngine import analytic, MBIR
+from pyMBIR.utils import apply_proj_tilt
+from readNeutronData import * 
+from tomopy.misc.corr import median_filter
+import os
+import dxchange
+
 
 
 class Worker(QObject):
@@ -90,6 +96,126 @@ def create_circle_mask(y,x,center,rad):
     mask = (x-center[0])*(x-center[0]) + (y-center[1])*(y-center[1]) <= rad*rad
     return mask
     
+def MBIR_fromGUI(input_params,gui_params):
+    '''
+    2 dictionaries from the GUI interface
+    input_params : Has all the user inputs including advanced params 
+    gui_params : Broad cast arrays and other params that are required for GUI code 
+    '''
+    
+    max_core = input_params['max_core']
+    num_wav_level = input_params['wav_level']
+    tot_col = input_params['tot_col']
+    num_col = input_params['num_col']
+    rot_center = input_params['rot_center']
+    
+    rec_params={}
+    rec_params['debug']=False
+    rec_params['verbose']=True
+    rec_params['num_iter']=input_params['max_iter']
+    rec_params['gpu_index']=range(0,input_params['num_gpu'])
+    rec_params['MRF_P']=input_params['mrf_p']
+    rec_params['MRF_SIGMA']=input_params['mrf_sigma']
+    rec_params['stop_thresh']=input_params['stop_thresh']
+
+    #GUI related params 
+    rec_params['gui_emit']=True
+    rec_params['emit_freq']=gui_params['emit_freq']
+    rec_params['sent_recon_array']=gui_params['sent_reconstructed_array']
+    rec_params['progress']=gui_params['progress']
+    rec_params['finished']=gui_params['finished']
+
+    #rec_params['filt_type']='Ram-Lak'
+    #rec_params['filt_cutoff']=f_c
+
+    #Miscalibrations - detector offset, tilt
+    miscalib={}
+    miscalib['delta_u']=(tot_col/2 - rot_center)
+    miscalib['delta_v']=0
+    miscalib['phi']=0 
+
+    z_start = input_params['z_start']
+    z_numSlice = input_params['z_numSlice']
+    filt_size=input_params['med_filt_win'] #window of median filter 
+    print(os.path.join(input_params['brt_path'],'*.tiff'))
+    #Read data 
+    brights=readMedianTifDir(os.path.join(input_params['brt_path'],'*.tiff'),z_start,z_numSlice,filt_size)
+    darks=readMedianTifDir(os.path.join(input_params['drk_path'],'*.tiff'),z_start,z_numSlice,filt_size)
+    count_data=readRadiographTifDir2(input_params['data_path'],z_start,z_numSlice)
+    angles =get_angles(input_params['data_path']) 
+
+    #Pre-process 
+    print('Applying median filter to remove gammas')
+    count_data= median_filter(count_data,size=filt_size,axis=0,ncore=max_core)
+
+    print(count_data.shape)
+    print(brights.shape)
+    print(darks.shape)
+
+    #Normalize and correct 
+    norm_data = -np.log((count_data - darks) / (brights - darks))
+    count_data[np.isnan(norm_data)]=0 #remove the bad values
+    count_data[np.isinf(norm_data)]=0 #remove the bad values 
+    norm_data[np.isnan(norm_data)]=0
+    norm_data[np.isinf(norm_data)]=0
+
+    #More pre-process 
+    norm_data = remove_stripe_fw(norm_data,level=num_wav_level,ncore=max_core)
+
+    if(input_params['use_det_tilt'] == 'True'):
+        print('Tilt axis correction ..')
+        count_data=apply_proj_tilt(count_data,input_params['det_tilt'],ncore=max_core)
+        norm_data=apply_proj_tilt(norm_data,input_params['det_tilt'],ncore=max_core)
+
+    ####Crop Data along the detector column axis #####
+    count_data= count_data[:,:,tot_col//2-num_col//2:tot_col//2+num_col//2]
+    norm_data = norm_data[:,:,tot_col//2-num_col//2:tot_col//2+num_col//2]
+    ######End of data cropping #######
+    
+    norm_data=norm_data.swapaxes(0,1)
+    count_data=count_data.swapaxes(0,1)
+    
+    #For data set 1
+    print('Subsetting data ..')
+    ang_idx = range(0,len(angles),input_params['view_subsamp'])
+    count_data=count_data[:,ang_idx,:]
+    norm_data = norm_data[:,ang_idx,:] #Subset to run on GPU 
+    angles = angles[ang_idx]
+    
+    print('Shape of data array')
+    print(count_data.shape)
+    
+    proj_data = norm_data 
+    
+    det_row,num_angles,det_col=proj_data.shape
+    proj_dims=np.array([det_row,num_angles,det_col])
+    
+    proj_params={}
+    proj_params['type'] = 'par'
+    proj_params['dims']= proj_dims
+    proj_params['angles'] = angles*np.pi/180
+    proj_params['forward_model_idx']=2
+    proj_params['alpha']=[np.pi/2]
+    proj_params['pix_x']=input_params['det_x']
+    proj_params['pix_y']=input_params['det_y']
+    
+    vol_params={}
+    vol_params['vox_xy']=input_params['vox_xy']
+    vol_params['vox_z']=input_params['vox_z']
+    vol_params['n_vox_z']=input_params['n_vox_z'] 
+    vol_params['n_vox_y']=input_params['n_vox_y'] 
+    vol_params['n_vox_x']=input_params['n_vox_x'] 
+
+    print('Starting MBIR..')
+    rec_mbir=np.float32(MBIR(proj_data,count_data,proj_params,miscalib,vol_params,rec_params))
+
+    if input_params['write_op'] == 'True':
+        temp_str = 'mbir' 
+        temp_path=os.path.join(input_params['op_path'],temp_str)
+        dxchange.write_tiff_stack(rec_mbir, fname=temp_path, start=z_start,overwrite=True)
+
+
+'''
 def full_recon_testdata(progress,finished,sent_recon_array):
     #Detector and experiment parameters
     num_angles = 256
@@ -229,121 +355,4 @@ def full_recon_testdata(progress,finished,sent_recon_array):
     rec_params['filt_cutoff']=0.8
     
     recon_mbir=MBIR(proj_data,weight_data,proj_params,miscalib,vol_params,rec_params)
-
-def MBIR_fromGUI(input_params,gui_params):
-    '''
-    2 dictionaries from the GUI interface
-    input_params : Has all the user inputs including advanced params 
-    gui_params : Broad cast arrays and other params that are required for GUI code 
-    '''
-    
-    max_core = input_params['max_core']
-    num_wav_level = input_params['wav_level']
-    tot_col = input_params['tot_col']
-    num_col = input_params['num_col']
-    rot_center = input_params['rot_center']
-    
-    rec_params={}
-    rec_params['debug']=False
-    rec_params['verbose']=True
-    rec_params['num_iter']=input_params['max_iter']
-    rec_params['gpu_index']=range(0,input_params['num_gpu'])
-    rec_params['MRF_P']=input_params['mrf_p']
-    rec_params['MRF_SIGMA']=input_params['mrf_sigma']
-    rec_params['stop_thresh']=input_params['stop_thresh']
-
-    #GUI related params 
-    rec_params['gui_emit']=True
-    rec_params['emit_freq']=gui_params['emit_freq']
-    rec_params['sent_recon_array']=gui_params['sent_reconstructed_array']
-    rec_params['progress']=gui_params['progress']
-    rec_params['finished']=gui_params['finished']
-
-    #rec_params['filt_type']='Ram-Lak'
-    #rec_params['filt_cutoff']=f_c
-
-    #Miscalibrations - detector offset, tilt
-    miscalib={}
-    miscalib['delta_u']=(tot_col/2 - rot_center)
-    miscalib['delta_v']=0
-    miscalib['phi']=0 
-
-    z_start = input_params['z_start']
-    z_numSlice = input_params['z_numSlice']
-    filt_size=input_params['med_filt_win'] #window of median filter 
-    print(os.path.join(input_params['brt_path'],'*.tiff'))
-    #Read data 
-    brights=readMedianTifDir(os.path.join(input_params['brt_path'],'*.tiff'),z_start,z_numSlice,filt_size)
-    darks=readMedianTifDir(os.path.join(input_params['drk_path'],'*.tiff'),z_start,z_numSlice,filt_size)
-    count_data=readRadiographTifDir2(input_params['data_path'],z_start,z_numSlice)
-    angles =get_angles(input_params['data_path']) 
-
-    #Pre-process 
-    print('Applying median filter to remove gammas')
-    count_data= median_filter(count_data,size=filt_size,axis=0,ncore=max_core)
-
-    print(count_data.shape)
-    print(brights.shape)
-    print(darks.shape)
-
-    #Normalize and correct 
-    norm_data = -np.log((count_data - darks) / (brights - darks))
-    count_data[np.isnan(norm_data)]=0 #remove the bad values
-    count_data[np.isinf(norm_data)]=0 #remove the bad values 
-    norm_data[np.isnan(norm_data)]=0
-    norm_data[np.isinf(norm_data)]=0
-
-    #More pre-process 
-    norm_data = remove_stripe_fw(norm_data,level=num_wav_level,ncore=max_core)
-
-    if(input_params['use_det_tilt'] == 'True'):
-        print('Tilt axis correction ..')
-        count_data=apply_proj_tilt(count_data,input_params['det_tilt'],ncore=max_core)
-        norm_data=apply_proj_tilt(norm_data,input_params['det_tilt'],ncore=max_core)
-
-    ####Crop Data along the detector column axis #####
-    count_data= count_data[:,:,tot_col//2-num_col//2:tot_col//2+num_col//2]
-    norm_data = norm_data[:,:,tot_col//2-num_col//2:tot_col//2+num_col//2]
-    ######End of data cropping #######
-    
-    norm_data=norm_data.swapaxes(0,1)
-    count_data=count_data.swapaxes(0,1)
-    
-    #For data set 1
-    print('Subsetting data ..')
-    ang_idx = range(0,len(angles),input_params['view_subsamp'])
-    count_data=count_data[:,ang_idx,:]
-    norm_data = norm_data[:,ang_idx,:] #Subset to run on GPU 
-    angles = angles[ang_idx]
-    
-    print('Shape of data array')
-    print(count_data.shape)
-    
-    proj_data = norm_data 
-    
-    det_row,num_angles,det_col=proj_data.shape
-    proj_dims=np.array([det_row,num_angles,det_col])
-    
-    proj_params={}
-    proj_params['type'] = 'par'
-    proj_params['dims']= proj_dims
-    proj_params['angles'] = angles*np.pi/180
-    proj_params['forward_model_idx']=2
-    proj_params['alpha']=[np.pi/2]
-    proj_params['pix_x']=input_params['det_x']
-    proj_params['pix_y']=input_params['det_y']
-    
-    vol_params={}
-    vol_params['vox_xy']=input_params['vox_xy']
-    vol_params['vox_z']=input_params['vox_z']
-    vol_params['n_vox_z']=input_params['n_vox_z'] 
-    vol_params['n_vox_y']=input_params['n_vox_y'] 
-    vol_params['n_vox_x']=input_params['n_vox_x'] 
-
-    print('Starting MBIR..')
-    rec_mbir=np.float32(MBIR(proj_data,count_data,proj_params,miscalib,vol_params,rec_params))
-
-    if input_params['write_op'] == 'True':
-        temp_str = 'mbir' 
-        temp_path=os.path.join(input_params['op_path'],temp_str)
-        dxchange.write_tiff_stack(rec_mbir, fname=temp_path, start=z_start,overwrite=True)
+'''
